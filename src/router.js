6cd98c1cd335a75059c35382b6d4dd6678c2d2a3
novb1492 <novb1492@naver.com>
2021-10-05 11:25:38 +0900

    first

---
+++ src/router.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from './components/views/home.vue';
+
+Vue.use(VueRouter);
+
+const router =new VueRouter({
+    mode:'history',
+    routes: [
+      {path:'/',component: Home}  
+    ]
+});
+export default router;
