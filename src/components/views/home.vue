ad6990326a704a070ca4fa38e376b796337a1aed
novb1492 <novb1492@naver.com>
2021-10-05 11:58:09 +0900

    axios설치

--- src/components/views/home.vue
+++ src/components/views/home.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>ab</div>
+    <div>
+        {{price}}
+        <input type="button" onclick="reuqest()" value="클릭">
+    </div>
+    
+
 </template>
 <script>
+import Axios from 'axios';
+
+
 
 export default {
    name :'home',
+    data(){
+        return{
+            price:50,
+        }
+    },
+    methods :{
+        request() {
+            Axios.get('http://localhost:8080/auth/test').then(res => { console.log(res.data) });
+        }
+    },
 }
 </script>
