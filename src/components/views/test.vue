e50e5d68f875a754e33c00449c40206aa96825ae
novb1492 <novb1492@naver.com>
2021-10-10 11:17:49 +0900

    head에 srcript넣는법 이거였구나

--- src/components/views/test.vue
+++ src/components/views/test.vue
@@ -5,10 +5,12 @@
         <input type="button" @click="request" value="클릭">
         <a href="http://localhost:8080/spring/test">백엔드</a>
         <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+        <input type="button" @click="this.settle" value="settle">
     </div>
     
 
 </template>
+<script src="https://tbnpg.settlebank.co.kr/resources/js/SettlePG.js"></script>
 <script>
 import Axios from 'axios';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
@@ -44,11 +46,45 @@ export default {
             });
         },
         MyCustomUploadAdapterPlugin( editor ) {
-        editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
-        // Configure the URL to the upload script in your back-end here!
-        return new MyUploadAdapter( loader );
-        };
+            editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
+            // Configure the URL to the upload script in your back-end here!
+            return new MyUploadAdapter( loader );
+            };
+        },
+        settle(){
+
+            SETTLE_PG.pay({
+            "env": "https://tbnpg.settlebank.co.kr",
+            "mchtId": "nx_mid_il",
+            "method": "vbank",
+            "trdDt": 20211010,    
+            "trdTm": 110000,
+            "mchtTrdNo":123456789,
+            "mchtName": "세틀뱅크",
+            "mchtEName": "Settlebank",
+            "pmtPrdtNm": 'test',
+            "trdAmt": 1000,
+            "mchtCustId":'mchtCustId',
+            "notiUrl": "http://kim80800.iptime.org:8080/auth/settlebank",
+            "nextUrl": "https://localhost:8443/doneSettlebankPage.jsp",
+            "cancUrl": "https://localhost:8443/canceSettlePage.html",
+            "pktHash": 'pktHash',
+            "ui": {
+                "type": "popup",
+                "width": "430",
+                "height": "660"
+            }
+            }, function(rsp){
+                console.log('통신완료');
+                console.log(rsp);
+            });      
         }
     },
+    created(){
+        console.log('sss');
+        let recaptchaScript = document.createElement('script')
+        recaptchaScript.setAttribute('src', 'https://tbnpg.settlebank.co.kr/resources/js/SettlePG.js')
+        document.head.appendChild(recaptchaScript)
+    }
 }
 </script>
