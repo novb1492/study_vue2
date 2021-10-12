2f798f367725e19dc136213f09bcf24c3aba9289
novb1492 <novb1492@naver.com>
2021-10-12 21:47:03 +0900

    비밀번호 찾기 페이지구현

--- src/components/views/user/changePwdPage.vue
+++ src/components/views/user/changePwdPage.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="changePwdPage">
-        
+        <div>새 비밀번호를 입력해주세요</div>
+        <input type="password" id="pwd" class="" placeholder="4자이상 10자 이하입니다">
+        <div>한번더 일치하게 입력해주세요</div>
+        <input type="password" id="pwd2" class="" placeholder="4자이상 10자 이하입니다">
+        <input type="button" value="변경" id="updateButton" @click="this.tryUpdatePwd">
     </div>
 </template>
 <script>
@@ -9,10 +13,29 @@ export default {
     name:'changePwdPage',
     data(){
         return {
-            object:module.getParam('object')
+            
         }
     },
     methods:{
+        tryUpdatePwd(){
+             var parms=module.getAllParam();
+            var pwd=document.getElementById('pwd').value;
+            var pwd2=document.getElementById('pwd2').value;
+            let data=JSON.stringify({
+                "pwd":pwd,
+                "pwd2":pwd2,
+                "token":parms.object,
+                "scope":"pwd",
+                "detail":"find"
+            });
+            var url='http://localhost:8080/user/change?scope=pwd'
+            module.requestPutToServer2(url,data).then(result=>{
+                alert(result.message);
+                if(result.flag){
+                    location.href='/loginPage';
+                }
+            });
+        }
     },
     created(){
         var parms=module.getAllParam();
