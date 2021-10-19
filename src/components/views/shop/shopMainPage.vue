9d26ce7c4e31e81a5992e12d1c2ea356f51cf2cb
novb1492 <novb1492@naver.com>
2021-10-19 11:00:45 +0900

    img 태그는 :src

--- src/components/views/shop/shopMainPage.vue
+++ src/components/views/shop/shopMainPage.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="shopMainPage">
         <div id="productArea">
-            <img src="https://s3.ap-northeast-2.amazonaws.com/wonderlandimgs/productimgs/스크린샷(1972).png" @click="this.test">
+            <span v-for="product in this.products" :key="product">
+                <img :src="product.imgPath">
+                <span> {{product.price}} dsds</span>
+            </span>
         </div>
     </div>
 </template>
 <style>
 #productArea{position: absolute; top: 100px; left: 20px;}
-img{width: 100px;height: 100px;}
+img{width: 150px;height: 100px;}
 </style>
 <script>
 import * as module from "../../jslib/jslib.js";
@@ -15,18 +18,19 @@ export default {
    name :'firstdoor',
    data() {
        return {
-           products:this.getProduct('coffee',1,null)
+           products:[]
        }
    },
    methods: {
       getProduct(kind,page,keyword){
           module.requestGetToServer('http://localhost:8080/product/select?kind='+kind+'&page='+page+'&keyword='+keyword).then(result=>{
               console.log(result);
+              this.products=result.products;
           });
-      }
+      },
    },
    created() {
-       
+       this.getProduct('coffee',1,null)
    },
    
 }
